3440d3f5c0969039a638e8997e23fb815c974ae6
Owez <root@ogriffiths.com>
2020-12-31 21:24:23 +0000

    Added infomation link

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,16 @@ extern crate rocket;
 mod tv;
 mod utils;
 
+use utils::ResponseModel;
+
+#[get("/")]
+fn index() -> ResponseModel<()> {
+    ResponseModel::basic(200, format!("Microservice called 'tagzen' on v{} made for tagging television shows and movies made by https://ogriffiths.com", env!("CARGO_PKG_VERSION")))
+}
+
 fn main() {
     rocket::ignite()
         .attach(rocket_contrib::helmet::SpaceHelmet::default())
-        .mount("/", routes![tv::single])
+        .mount("/", routes![index, tv::single])
         .launch();
 }
