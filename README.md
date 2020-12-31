9944cf801042c63c91feb70319487c3ca318ed3a
Owez <root@ogriffiths.com>
2020-12-31 23:31:25 +0000

    Added docs

---
+++ README.md
@@ -0,0 +1,7 @@
+# tagzen
+
+An api microservice to tag and catagorise tv & movies at ease
+
+## Running
+
+Clone then build with `cargo build --release`. The outputted binary from this will be outputted inside of `target/release` which can be optionally stripped and ran as a static binary. All help infomation is contained inside of the api itself, so just visit the index page once it's up and running 👐
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+//! An api microservice to tag and catagorise tv & movies at ease
+//! 
+//! # Running
+//! 
+//! Clone then build with `cargo build --release`. The outputted binary from this
+//! will be outputted inside of `target/release` which can be optionally stripped
+//! and ran as a static binary. All help infomation is contained inside of the api
+//! itself, so just visit the index page once it's up and running 👐
+
 #![feature(proc_macro_hygiene, decl_macro)]
 
 /// Crate version
