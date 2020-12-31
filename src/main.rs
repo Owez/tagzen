8cc1e126398b4676069b4bfb44fe9ef2edbaeaea
Owez <root@ogriffiths.com>
2020-12-31 23:17:51 +0000

    Updated index help info again

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ mod utils;
 
 #[get("/")]
 fn index() -> String {
-    format!("ROUTE /\n\n\nAbout\n    Microservice api for tagging television shows and movies for use publically\n    for free, forever. Created by https://ogriffiths.com. Help is available for\n    each route and endpoint on GET access. Running on v{} currently.\n\n\nChild routes/endpoints\n    - /tv: Television show tagging, allowing single episode or seasonal tagging", VERSION)
+    format!("ROUTE /\n\n\nAbout\n    Microservice api for tagging television shows and movies for use publically\n    for free, forever. Created by https://ogriffiths.com. Help is available for\n    each route and endpoint on GET access. Running on v{} currently with the\n    foss repository contained inside of https://github.com/owez/tagzen/.\n\n\nChild routes/endpoints\n    - /tv: Television show tagging, allowing single episode or seasonal tagging", VERSION)
 }
 
 fn main() {
